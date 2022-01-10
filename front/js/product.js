bb024b2fe66a15b1ce719ce5d49495160533240b
chnaww <tnduong.vn@gmail.com>
2022-01-10 19:22:20 +0100

    refactoring

--- front/js/product.js
+++ front/js/product.js
@@ -1,42 +1,58 @@
-let params = new URL(document.location).searchParams;
-let idProduct = params.get("id");
-const colorPicked = document.getElementById("colors");
-const qtyPicked = document.getElementById('quantity');
+//récupération de l'ID lié au canapé sélectionné
+let idURL = new URL(document.location).searchParams;
+let idProduct = idURL.get("id");
 
+//requet GET avec l'ID du canapé
 fetch('http://localhost:3000/api/products/' + idProduct)
-    .then(function(res) {
-        if (res.ok) {
-            return res.json();
+    .then(function(result) {
+        if (result.ok) {
+            return result.json();
         }
     })
-    .then(function(value) {
-        console.log(value);
-        document.getElementById('title').innerHTML = value.name;
-        document.getElementById('price').innerHTML = value.price;
-        document.getElementById('description').innerHTML = value.description;
+    .then (function(value) {
+        //récupération du nom du canapé
+        let productTitle = document.getElementById('title');
+        productTitle.innerHTML = value.name;
 
+        //récupération du prix du canapé
+        let productPrice = document.getElementById('price');
+        productPrice.innerHTML = value.price;
+
+        //récupération du description du canapé
+        let productDescription = document.getElementById('description');
+        productDescription.innerHTML = value.description;
+
+        //intégration de l'image du canapé
         let productImg = document.createElement("img");
-        document.querySelector(".item__img").appendChild(productImg);
+        let selectImg = document.querySelector(".item__img");
+
+        selectImg.appendChild(productImg);
+
+        //attribut de l'image
         productImg.src = value.imageUrl;
         productImg.alt = value.altTxt;
 
-        for ( let i of value.colors) {
-        let newOption = document.createElement("option");
-        document.getElementById("colors").appendChild(newOption);
-        newOption.value = i;
-        newOption.innerHTML = i;
+        //boucle pour afficher les différents choix de couleur
+        for ( let color of value.colors) {
+            let newOption = document.createElement("option");
+            let selectOption = document.getElementById("colors");
+
+            selectOption.appendChild(newOption);
+            newOption.value = color;
+            newOption.innerHTML = color;
         }
 
-        let btnCart = document.getElementById("addToCart");
-        btnCart.addEventListener('click', function() {
-            if (qtyPicked.value > 0  && qtyPicked.value <=100 && qtyPicked !=0) {
+        //récupération des données pour le localcache
+        let optionProducts = {
+            productID : value._id,
+            productName : value.name,
+            productPrice : value.price,
+            productColor : colorPicked.value,
+            productQty : qtyPicked.value
+        }
 
-                
-            } else {
-                console.log("error");
-            }
-        })
+        console.log(optionProducts);
     })
     .catch(function(err) {
-
+        console.log("erreur de récupération API");
     });
--- front/js/script.js
+++ front/js/script.js
@@ -5,28 +5,38 @@ fetch("http://localhost:3000/api/products")
         }
     })
     .then(function(value) {  
-        console.log(value);
-        for (let i of value) {
-            let newA = document.createElement("a");
-            let newArticle = document.createElement("article");
-            let newImg = document.createElement("img");
-            let newH3 = document.createElement("h3");
-            let newP = document.createElement("p");
-            let elt = document.getElementById("items");
+        for (let item of value) {
+            //création du lien pour chaque canapé
+            let newA = document.createElement("a")
+            let eltItems = document.getElementById("items");
+            eltItems.appendChild(newA);
+            newA.setAttribute("href", "./product.html?id=" + item._id);
 
-            elt.appendChild(newA).appendChild(newArticle);
-            newA.setAttribute("href", "./product.html?id=" + i._id);
+            //création de son article
+            let newArticle = document.createElement("article");
+            newA.appendChild(newArticle);
+
+            //création et intégration de l'image du canapé
+            let newImg = document.createElement("img");
             newArticle.appendChild(newImg);
+            newImg.src = item.imageUrl;
+            newImg.alt = item.altTxt;
+
+            //création et inégration du nom du canapé
+            let newH3 = document.createElement("h3");
             newArticle.appendChild(newH3);
-            newArticle.appendChild(newP);
-            newImg.setAttribute("src", i.imageUrl);
-            newImg.setAttribute("alt", i.altTxt);
             newH3.setAttribute("class", "productName");
-            newH3.innerHTML = i.name;
+            newH3.innerHTML = item.name //display canapé's name
+
+            //création et intégration de la description du canapé
+            let newP = document.createElement("p");
+            newArticle.appendChild(newP);
             newP.setAttribute("class", "productDescription");
-            newP.innerHTML = i.description;
+            newP.innerHTML = item.description //display canapé's description
+
         }
     }) 
     .catch(function(err) {
-
+        //message d'erreur si la récupération d'API a échoué
+        console.log("error fetch API");
     });
